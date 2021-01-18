c9042ab4918de4f89b9158b61ceb007c18dc1918
Alfredo A Santoyo <santoyoalfredo@gmail.com>
2021-01-18 12:52:18 -0500

    Add Bootstrap

--- api/app.js
+++ api/app.js
@@ -6,8 +6,7 @@ var logger = require('morgan');
 var cors = require("cors");
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var APIRouter = require("./routes/API");
+var APIRouter = require("./routes/api");
 
 var app = express();
 
@@ -23,7 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use("/api", APIRouter);
 
 // catch 404 and forward to error handler
