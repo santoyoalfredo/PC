10aabe331ea63da65503997cd4a3f435ce3894c1
Alfredo A Santoyo <santoyoalfredo@gmail.com>
2021-01-18 12:07:35 -0500

    Link Express with React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Axios from "axios";
 
 function App() {
+  Axios({
+    method: "GET",
+    url: "http://localhost:9000/api",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then(res => {
+    console.log(res.data.message);
+  });
+
   return (
     <div className="App">
       <header className="App-header">
